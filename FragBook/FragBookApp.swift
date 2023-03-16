3643074f7f77e8eedd6fc03c00c1d8c634f60051
Jakub Piskiewicz <jakub@piskiewicz.org>
2023-03-16 17:10:26 +0100

    Initial Commit

---
+++ FragBook/FragBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FragBookApp.swift
+//  FragBook
+//
+//  Created by Jakub Piskiewicz on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FragBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
